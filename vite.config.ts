e6b09b545cce75287f5d5465712edd49ac73c3cf
Zawar1226 <fast85853@gmail.com>
2025-12-28 16:01:20 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: './',       // ye crucial hai
-  server: {
-    host: true,
-    port: 3002
-  }
-})
-
+  build: {
+    outDir: "dist",
+  },
+});
